4c2a6830540a680050179e2d4f440a7a16acf4bb
Chenxin Ma <machx9@gmail.com>
2017-09-27 23:45:53 -0400

    add

--- __init__.py
+++ __init__.py
@@ -1,10 +1,1 @@
-##################
-
-# @Author: 			   Chenxin Ma
-# @Email: 			   machx9@gmail.com
-# @Date:               2017-09-26 22:30:58
-# @Last Modified by:   Chenxin Ma
-# @Last Modified time: 2017-09-26 22:31:33
-
-##################
-
+Some text here.
